4f78d7b79ae88261e9b9c4c8eb8492aea4350d5a
Mutan0105 <1290453107@qq.com>
2018-04-10 21:37:31 +0800

    1

--- 11_9.py
+++ 11_9.py
@@ -1,3 +1,11 @@
-#!/usr/bin/env python
-# encoding: utf-8
-print 'test'
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+a = [1,5,9,12,5,7]
+def add(x, y):
+    return x + y
+
+def average():
+    b = float(reduce(add,a)) / len(a)
+    print b
+
+average()
