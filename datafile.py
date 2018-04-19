7f5c37da52157bd1a247ed7e5bd54e180cd22aba
Mutan0105 <1290453107@qq.com>
2018-04-19 15:55:07 +0800

    class1

---
+++ datafile.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from datetime import datetime
+import shelve, os
+
+
+class userdatabase(object):
+    def __init__(self, dbfile):
+        self.db = {}
+        if os.path.exists(dbfile):
+            self.db = shelve.open(dbfile, 'c')
+        self.dbfile = dbfile
+        self.flag = False
+
+    def __del__(self):
+        data = shelve.open(self.dbfile, 'c')
+        data.update(self.db)
+        data.close()
+
+    def login(self, user, pwd):
+        if not self.db.has_key(user):
+            self.flag = False
+        elif self.db[user][0] == pwd:
+            self.db[user][1] = datetime.now()
+            self.flag = True
+
+    def deluser(self, user):
+        if self.flag:
+            self.db.pop(user)
+        else:
+            print 'login first'
+
+    def updateuser(self, user, pwd):
+        if self.flag:
+            self.db[user] = [pwd, datetime.now()]
+        else:
+            print 'login first'
+
+    def listall(self):
+        if self.flag:
+            for user in self.db:
+                print user, self.db[user][0], self.db[user][1]
+        else:
+            print 'login first'
+
+
+if __name__ == '__main__':
+    user = userdatabase("shelvedata")
+    user.login('root', 'root')
+    user.updateuser('test1', 'test1')
+    user.updateuser('test2', 'test2')
+    user.listall()
