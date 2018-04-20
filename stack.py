edf5d51df5abe2f39e914b3316045dd68295a21f
Mutan0105 <1290453107@qq.com>
2018-04-20 16:45:07 +0800

    timedisplay

---
+++ stack.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+class stack(object):
+    def __init__(self,list_a):
+        self.list_a = list_a
+
+    def push(self,val):
+        print self.list_a.append(val)
+
+
+    def pop(self,val2):
+        print self.list_a.remove(val2)
+
+
+    def isempty(self):
+        flag = len(self.list_a)
+        if flag == 0:
+            print 0
+        else:
+            print 1
+
+
+if __name__ == '__main__':
+    run = stack([1,3])
+    run.push(7)
+    run.push(2)
+    run.pop(2)
+    run.isempty()
