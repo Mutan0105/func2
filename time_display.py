db4b1c7bc3332486ba64e20988ddd4a2beb9239d
Mutan0105 <1290453107@qq.com>
2018-04-20 13:45:59 +0800

    timedisplay

--- time_display.py
+++ time_display.py
@@ -3,6 +3,7 @@
 import time
 
 class TimeDisplay(object):
+    'update()方法可以自由选择系统默认时间或者用户自己给出时间，display()方法可以选择时间显示格式'
     def __init__(self):
         pass
     def update(self):
@@ -14,12 +15,16 @@ class TimeDisplay(object):
 
         else:
             time_now = int(time.time())
-            self.time_local = time.localtime(time_now)
-
+            time_local = time.strftime('%Y-%m-%d', time.localtime(time_now))#将time模块获取的默认时间转换为年-月-日的格式
+            self.year = time_local[0:4]
+            self.month = time_local[5:7]
+            self.day = time_local[8:]
+            # 将上述已经按特点格式转换完毕的系统默认时间，用切片的方法获得年，月，日。赋值后方便在display()方法里将
+            # 系统默认或者从用户那里获得的输入用同一种方法进行不同格式输出
     def display(self):
         way = raw_input('采用系统默认默认设置请输入S，自定义请输入SL')
         if way.lower() == 's':
-            print time.strftime('%Y-%m-%d', self.time_local)
+            print '时间为：%s %s, %s' % (self.month,self.day, self.year)#系统默认格式输出
         else:
             print "输入'MDY'来按照 MM/DD/YY来显示\n输入'MDYY'来按照 MM/DD/YYYY来显示\n" \
                   "输入'DMY'来按照 DD/MM/YYYY来显示\n输入'DMYY'来按照 DD/MM/YYYY来显示"
